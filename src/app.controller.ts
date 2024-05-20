a11d9ff3654bd6bd55df8fa29eef46f8200a81b8
pauwelcz <pavel.sedlar1993@gmail.com>
2024-05-20 20:27:24 +0200

    feat: init data resources

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { DataService } from './data/data.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly dataService: DataService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.dataService.findAll();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [],
+  imports: [DataModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
