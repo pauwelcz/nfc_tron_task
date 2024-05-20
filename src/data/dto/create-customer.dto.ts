a2058c86c7f266839d3b6f019c9d912f5b6f8f61
pauwelcz <pavel.sedlar1993@gmail.com>
2024-05-20 23:52:06 +0200

    feat: validation dto

--- src/data/dto/create-customer.dto.ts
+++ src/data/dto/create-customer.dto.ts
@@ -1,7 +1,15 @@
+import { IsEmail, IsString, Length } from '@nestjs/class-validator';
+
 export class CreateCustomerDto {
+  @IsString()
+  @Length(1, 50)
   firstname: string;
 
+  @IsString()
+  @Length(1, 50)
   lastname: string;
 
+  @IsEmail()
+  @Length(1, 255)
   email: string;
 }
