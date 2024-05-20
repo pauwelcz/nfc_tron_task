a265b68d83c0b1a6439e28d9341932302d65d53a
pauwelcz <pavel.sedlar1993@gmail.com>
2024-05-20 20:44:40 +0200

    feat: add routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { DataService } from './data/data.service';
+import { CreateCustomerDto } from './data/dto/create-customer.dto';
+import { UpdateCustomerDto } from './data/dto/update-customer.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly dataService: DataService) {}
 
   @Get()
-  getHello(): string {
+  findAll(): string {
     return this.dataService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.dataService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCustomerDto: CreateCustomerDto) {
+    return this.dataService.create(createCustomerDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
+    return this.dataService.update(id, updateCustomerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.dataService.remove(id);
   }
 }
