68fa257703b34622e5b0e6c2a366a824b40c49c6
pauwelcz <pavel.sedlar1993@gmail.com>
2024-05-21 15:49:30 +0200

    perf: forbid non existing values

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Customers example')
